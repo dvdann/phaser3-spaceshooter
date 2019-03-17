4c8b4a39ff2e2ae299eb65a22844dd63c90776db
ReydVires <ahmad.fire@gmail.com>
2019-03-18 00:59:07 +0700

    remove empty space

--- src/index.js
+++ src/index.js
@@ -12,21 +12,21 @@ scenes.push(SceneGameOver);
 
 window.onload = () => {
   let config = {
-      type: Phaser.AUTO,
-      parent: 'phaser-game',
-      width: 480,
-      height: 640,
-      backgroundColor: 'black',
-      pixelArt: true,
-      seed: [seedRand],
-      physics: {
-    		default: 'arcade',
-    		arcade: {
-    			gravity: {x: 0, y: 0},
-    			debug: false
-    		}
-      },
-      scene: scenes
+    type: Phaser.AUTO,
+    parent: 'phaser-game',
+    width: 480,
+    height: 640,
+    backgroundColor: 'black',
+    pixelArt: true,
+    seed: [seedRand],
+    physics: {
+  		default: 'arcade',
+  		arcade: {
+  			gravity: {x: 0, y: 0},
+  			debug: false
+  		}
+    },
+    scene: scenes
   };
   game = new Phaser.Game(config);
 };
